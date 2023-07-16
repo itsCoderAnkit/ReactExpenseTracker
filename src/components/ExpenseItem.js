741d32a759405167abfa3e69e45b5deb1607d6e2
ANKIT <itsankitagarwal000@gmail.com>
2023-07-16 13:22:19 +0530

    first custom component made

---
+++ src/components/ExpenseItem.js
@@ -0,0 +1,11 @@
+function ExpenseItem() {
+    return (<div>
+        <h2>Expense Item!</h2>
+        <p>Food rs 10</p>
+        <p>Movie rs 200</p>
+        <p>petrol rs 10</p>
+        </div>)
+
+}
+
+export default ExpenseItem
