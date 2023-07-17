f8f0c35c83fafbeb30d2a404aa53069762e0ae77
ANKIT <itsankitagarwal000@gmail.com>
2023-07-17 10:04:25 +0530

    added filter

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,34 +1,20 @@
 import './Expenses.css'
 import ExpenseItem from './ExpenseItem'
 import Card from '../UI/Card'
+import React, { useState } from 'react'
+import ExpensesFilter from './ExpensesFilter';
 
 function Expenses(props){
-    // const expenses = [
-    //     {
-    //       id: 'e1',
-    //       title: 'Toilet Paper',
-    //       amount: 94.12,
-    //       date: new Date(2020, 10, 14),
-    //       location:"Delhi"
-    //     },
-    //     { id: 'e2', title: 'New TV', amount: 799.49, date: new Date(2021, 9, 12),location:"Mumbai" },
-    //     {
-    //       id: 'e3',
-    //       title: 'Car Insurance',
-    //       amount: 294.67,
-    //       date: new Date(2021, 8, 28),
-    //       location:"Kolkata"
-    //     },
-    //     {
-    //       id: 'e4',
-    //       title: 'New Desk (Wooden)',
-    //       amount: 450,
-    //       date: new Date(2021, 7, 12),
-    //       location:"Madras"
-    //     },
-    //   ];
+    
+
+    const [filteredYear, setFilteredYear] = useState('2020');
+
+    const filterChangeHandler = selectedYear => {
+      setFilteredYear(selectedYear);
+    };
     return (
         <Card className="expenses">
+            <ExpensesFilter selected={filteredYear} onChangeFilter={filterChangeHandler} />
             <ExpenseItem title={props.item[0].title} amount={props.item[0].amount} date={props.item[0].date}location={props.item[0].location}></ExpenseItem>
       <ExpenseItem title={props.item[1].title} amount={props.item[1].amount} date={props.item[1].date}location={props.item[1].location}></ExpenseItem>
       <ExpenseItem title={props.item[2].title} amount={props.item[2].amount} date={props.item[2].date}location={props.item[2].location}></ExpenseItem>
