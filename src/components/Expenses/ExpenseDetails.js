754a1cea792c514c0b6b74ab613821d29e35e023
ANKIT <itsankitagarwal000@gmail.com>
2023-07-17 10:13:26 +0530

    using react States

--- src/components/Expenses/ExpenseDetails.js
+++ src/components/Expenses/ExpenseDetails.js
@@ -1,20 +1,29 @@
 import './ExpenseItem.css'
+import React, {useState} from 'react'
 
 function ExpenseDetails(props){
+
+    const [title,setTitle] = useState(props.title)
+    const [amount, setAmount] = useState(props.amount)
+
     function clickHandler(){
-        console.log('clicked!!!!')
+        setTitle('clicked')
+        console.log(title)
+       // console.log('clicked!!!!')
     }
 
-    function deleteExpense(){
-        console.log('DELETE EXPENSE')
+    function changeExpense(){
+        setAmount(100)
+        console.log(amount)
+        console.log('CHANGE EXPENSE')
     }
     return(
         <div className ="expense-item__description">
-                <h2>{props.title}</h2>
-                <div className ="expense-item__price"> Rs {props.amount}</div>
+                <h2>{title}</h2>
+                <div className ="expense-item__price"> Rs {amount}</div>
                 <h2>{props.location}</h2>
                 <button onClick={clickHandler}>CLICK ME</button>
-                <button onClick={deleteExpense}>DELETE EXPENSE </button>
+                <button onClick={changeExpense}>CHANGE EXPENSE </button>
             </div>
     )
 }
