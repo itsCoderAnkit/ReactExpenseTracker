bd34a28e32888a942422391354956d1ec0e81706
ANKIT <itsankitagarwal000@gmail.com>
2023-07-17 10:23:38 +0530

    components with state created

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@
 import './App.css';
 //import ExpenseItem from './components/ExpenseItem';
 import Expenses from './components/Expenses/Expenses';
-
+import NewExpense from './components/NewExpense/NewExpense';
 function App() {
   const expenses = [
     {
@@ -32,31 +32,7 @@ function App() {
 
   return (
     <div className="App">
-      {/* <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header> */}
-
-
-
-      <h2> Lets get Started</h2>
-      {/* for(i=0; i<2 ;i++){
-        <ExpenseItem title={expenses[i].title} amount={expenses[i].amount} date={expenses[i].date} location={expenses[i].location}></ExpenseItem>
-      } */}
-      {/* <ExpenseItem title={expenses[0].title} amount={expenses[0].amount} date={expenses[0].date}location={expenses[0].location}></ExpenseItem>
-      <ExpenseItem title={expenses[1].title} amount={expenses[1].amount} date={expenses[1].date}location={expenses[1].location}></ExpenseItem>
-      <ExpenseItem title={expenses[2].title} amount={expenses[2].amount} date={expenses[2].date}location={expenses[2].location}></ExpenseItem>
-      <ExpenseItem title={expenses[3].title} amount={expenses[3].amount} date={expenses[3].date} location={expenses[3].location}></ExpenseItem> */}
+      <NewExpense></NewExpense>
       <Expenses item={expenses}></Expenses>
 
     </div>
