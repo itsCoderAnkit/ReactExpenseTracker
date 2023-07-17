58de08fcc63dc6098ba7838e4f57400ef8680d95
ANKIT <itsankitagarwal000@gmail.com>
2023-07-17 09:42:06 +0530

    event listener added

--- src/components/Expenses/ExpenseDetails.js
+++ src/components/Expenses/ExpenseDetails.js
@@ -1,11 +1,20 @@
 import './ExpenseItem.css'
 
 function ExpenseDetails(props){
+    function clickHandler(){
+        console.log('clicked!!!!')
+    }
+
+    function deleteExpense(){
+        console.log('DELETE EXPENSE')
+    }
     return(
         <div className ="expense-item__description">
                 <h2>{props.title}</h2>
                 <div className ="expense-item__price"> Rs {props.amount}</div>
                 <h2>{props.location}</h2>
+                <button onClick={clickHandler}>CLICK ME</button>
+                <button onClick={deleteExpense}>DELETE EXPENSE </button>
             </div>
     )
 }
